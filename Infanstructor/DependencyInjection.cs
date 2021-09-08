2d8284b92ecbaaa75fbd5e54cff9d7ab212d709e
desertfoox <salar.nili097@gmail.com>
2021-09-08 13:35:39 +0430

    BUILD/PROJECT

---
+++ Infanstructor/DependencyInjection.cs
@@ -0,0 +1,52 @@
+﻿using Infanstructor.Persistence;
+using Infanstructor.UserIdentity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
+namespace Infanstructor
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfanstructor(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                         options.UseSqlServer(
+                             configuration.GetConnectionString("maindb"),
+                             b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+
+     // Adding Jwt Bearer  
+     .AddJwtBearer(options =>
+     {
+         options.SaveToken = true;
+         options.RequireHttpsMetadata = false;
+         options.TokenValidationParameters = new TokenValidationParameters()
+         {
+             ValidateIssuer = true,
+             ValidateAudience = true,
+             ValidAudience = configuration["JWT:ValidAudience"],
+             ValidIssuer = configuration["JWT:ValidIssuer"],
+             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+         };
+     });
+
+            return services;
+        }
+    }
+}
